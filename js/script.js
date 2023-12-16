df619059f368358406d1f3b1892190a570142d6a
ptilege <ptilege@gmail.com>
2023-12-17 01:11:43 +0530

    email and name validation , Message Part

--- js/script.js
+++ js/script.js
@@ -41,39 +41,46 @@ sr.reveal('footer ',{delay:750, origin: 'bottom'});
 sr.reveal('footer h4',{delay:1000, origin: 'left'});
 
 
+emailjs.init("YN-ZdX4ac7TAQXR-j");
+
 function sendEmail() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
     
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
+    if(!email.trim() || !name.trim()){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Enter Both e-mail and Name",
+            footer: '<a href="index.html">Go to home</a>'
+          });
+        return;
+    }
 
-    // Create a FormData object to send form data
-    var formData = new FormData();
-    formData.append("name", name);
-    formData.append("email", email);
-    formData.append("message", message);
+    const templateParams = {
+        to_name: "Recipient Name",
+        from_name: name,
+        from_email: email,
+        message: message,
+    };
 
-    // Make an AJAX request
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "sendmail.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // Update content type
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                var response = JSON.parse(xhr.responseText);
-                if (response.status === "success") {
-                    alert("Email sent successfully!");
-                } else {
-                    alert("Error sending email. Please try again.");
-                }
-            } else {
-                console.error("Error:", xhr.statusText);
-            }
-        }
-    };
-    xhr.onerror = function () {
-        console.error("Request failed");
-    };
-    xhr.send(formData);
+    emailjs.send("service_rtxy2en", "template_ybdjd5d", templateParams)
+        .then(function(response) {
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Message Sent",
+                showConfirmButton: false,
+                timer: 1500
+              });
+        }, function(error) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Message Not Sent",
+                footer: '<a href="index.html">Go to home</a>'
+              });
+        });
 }
 
